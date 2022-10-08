2755c3cf51f8038c28b30944af111f365cf2a0e3
GattuP07 <gautam.patoliya@toforzero.in>
2022-10-08 11:09:57 +0530

    animation changes

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,42 +3,44 @@ import { CardData } from './ProjectCards';
 import ScrollAnimation from 'react-animate-on-scroll';
 import "animate.css/animate.min.css";
 
-export const Projects = () => {
-    return (
-        <div className="w-full text-white mt-16 pb-10">
-            <section className="container px-3 mx-auto lg:max-w-7xl md:px-8">
-                <ScrollAnimation
-                    animateOnce={true}
-                    animateIn="animate__jackInTheBox"
-                    duration={1.2}
-                >
-                    <p className='title text-gradient'>My Projects</p>
-                </ScrollAnimation>
+export const Projects = () => (
 
-                <div>
-                <ScrollAnimation
-                    animateOnce={true}
-                    animateIn="animate__slideInUp"
-                    duration={1.2}
-                >
+    <div className="w-full text-white mt-16 pb-10">
+        <section className="container px-3 mx-auto lg:max-w-7xl md:px-8">
+            <ScrollAnimation
+                animateOnce={true}
+                animateIn="animate__jackInTheBox"
+                duration={1.2}
+            >
+                <p className='title text-gradient'>My Projects</p>
+            </ScrollAnimation>
 
-                        <div className="grid grid-cols-3 gap-5">
+            <div>
 
-                            {CardData.map((card) => (
+                <div className="grid grid-cols-3 gap-5">
 
-                                <div className={`main-card-p h-[15rem] rounded-lg bg-cover bg-center hover:-translate-y-2 transition duration-300 ${card.bg}`} key={card.value}>
-                                    <div className="card-p flex justify-center items-center h-[15rem] group rounded-lg text-center transition-every duration-500 hover:bg-card-overlay">
-                                        <p className='card-text invisible text-lg z-50 group-hover:visible'>{card.text}</p>
-                                    </div>
+                    {CardData.map((card) => (
+
+                        // <ScrollAnimation
+                        //     animateOnce={true}
+                        //     animateIn="animate__slideInLeft"
+                        //     duration={1.2}
+                        //     delay={2}
+                        // >
+                            <div className={`main-card-p h-[15rem] rounded-lg bg-cover bg-center hover:-translate-y-2 transition duration-300 ${card.bg}`} key={card.value}>
+                                <div className="card-p flex justify-center items-center h-[15rem] group rounded-lg text-center transition-every duration-500 hover:bg-card-overlay">
+                                    <p className='card-text invisible text-lg z-50 group-hover:visible'>{card.text}</p>
                                 </div>
+                            </div>
 
-                            ))}
+                        // </ScrollAnimation>
+                        
+                    ))}
 
-                        </div>
-                </ScrollAnimation>
-                    </div>
+                </div>
+            </div>
 
-            </section>
-        </div>
-    )
-}
+        </section>
+    </div>
+
+)
